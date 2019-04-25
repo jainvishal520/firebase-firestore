6968e7380ddcefbd22c2de200a668c1c6ba2b9af
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-25 23:19:05 +0530

    saving data to firestore db

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ db.collection('cafes').get().then((snapshot) => {
 })
 
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
@@ -20,7 +21,16 @@ function renderCafe(doc){
     li.appendChild(city);
 
     cafeList.appendChild(li);
-
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    })
+    form.name.value = '';
+    form.city.value = '';
+})
 
 
