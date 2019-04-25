f78d23a4a2bee67fc07ae8c69325f22df93c0012
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-25 23:31:33 +0530

    delete data

--- app.js
+++ app.js
@@ -8,19 +8,31 @@ db.collection('cafes').get().then((snapshot) => {
 
 const cafeList = document.querySelector('#cafe-list');
 const form = document.querySelector('#add-cafe-form');
+
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
+
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
+
 
     cafeList.appendChild(li);
+
+    cross.addEventListener('click', (e) => {
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+
 }
 
 form.addEventListener('submit', (e) => {
